3313a34358240666913b18d664a07e5c0a72540e
Sri Nambudiripad <srinambudiripad@gmail.com>
2023-02-08 22:26:30 -0600

    added phone number success verification page

--- LastBiteSwift/SplashPage.swift
+++ LastBiteSwift/SplashPage.swift
@@ -1,37 +1,21 @@
+
 import SwiftUI
 
 struct SplashPage: View {
     var body: some View {
-        VStack {
-            Image("Food Transparent")
-            Image("Logo")
-                .padding(.bottom, 16)
-            Image("slogan")
-                .padding(.bottom, 40)
-            Button(action: {
-                            // Button action here
-                        }) {
-                            Text("Log In")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding(EdgeInsets(top: 10, leading: 90, bottom: 10, trailing: 90))
-                                .background(Color.pink)
-                                .cornerRadius(10)
-                        }
-                        
-                        Text("Sign up")
-                            .font(.subheadline)
-                            .foregroundColor(.blue)
-                            .padding(.top)
-                    }
+        ZStack {
+            Image("Splash")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+        }
     }
 }
-
-
 
 struct SplashPage_Previews: PreviewProvider {
     static var previews: some View {
         SplashPage()
     }
+    
 }
-
---
+++ LastBiteSwift/SuccessView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct SuccessView: View {
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Spacer().frame(height : 86)
+            
+            Text("Success!")
+                .font(.custom("DMSans-Bold", size: 50))
+                .bold()
+            
+            Text("You've successfully verified your account! We've also sent you a confirmation to your email.")
+                .font(.custom("DMSans", size: 20))
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Image("Success")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:120,height:120)
+            
+            
+            Spacer()
+            
+            Button(action: {}) {
+               Text("Grab the Last Bite")
+                  .frame(width: 358, height: 51)
+                  .background(Color("AccentColor"))
+                  .foregroundColor(.white)
+                  .cornerRadius(100)
+                  .font(.custom("DMSans-Bold", size:16))
+            }
+            
+            
+        }.padding()
+    }
+}
+
+
+
+struct SuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuccessView()
+    }
+}
