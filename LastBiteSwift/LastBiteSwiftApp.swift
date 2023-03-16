9711f9d091cdf99cbe22790ceefcc38ec05cea50
Jay Uppaluri <jay.uppaluri@gmail.com>
2023-03-16 18:21:56 -0500

    adding payment functionality, firebase functions for node backend to create payment intent

--- LastBiteSwift/LastBiteSwiftApp.swift
+++ LastBiteSwift/LastBiteSwiftApp.swift
@@ -7,11 +7,22 @@
 
 import SwiftUI
 import FirebaseCore
+import Stripe
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+      StripeAPI.defaultPublishableKey = "pk_test_51Mm5YMJa42zn3jCLDGyMInDAgUBxKBCNjFLeqCdpSi2QTwZwKFahXKbvd23gHy18En2nS3eqCfthgRPNEZwxZy4V00chCbTmqB"
+      FirebaseApp.configure()
+        
+    if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+        if let url = userActivity.webpageURL {
+            let stripeHandled = StripeAPI.handleURLCallback(with: url)
+            if (stripeHandled) {
+                return true;
+            }
+        }
+    }
 
     return true
   }
@@ -23,7 +34,7 @@ struct LastBiteSwiftApp: App {
     
     var body: some Scene {
         WindowGroup {
-            SplashScreenView()
+            CheckoutView()
         }
     }
 }
