f510118667b0fb0d28d3368991c84544ae4bb0dd
Jay Uppaluri <jay.uppaluri@gmail.com>
2023-04-20 17:50:47 -0500

    updating restaurant portal

--- restaurantportal/backend.py
+++ restaurantportal/backend.py
@@ -6,9 +6,11 @@ from firebase_admin import firestore
 from firebase_admin import auth
 import json
 from flask import Flask, render_template, Response, request, redirect, url_for
+from flask import jsonify
 
 app = Flask(__name__)
-cred = credentials.Certificate('C:\\Users\\srina\\Desktop\\lastbite_python\\serviceAccountKey.json')
+cred = credentials.Certificate(
+    'C:\\Users\\srina\\Desktop\\lastbite_python\\serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 
 
@@ -16,22 +18,168 @@ firebase_admin.initialize_app(cred)
 def index():
     return render_template('index.html')
 
+
 def get_firestore_database():
     return firestore.client()
 
+
 def get_orders(database):
     current_time = datetime.now()
-    two_weeks_ago = current_time - timedelta(days=14)
-    
-    return database.collection("orders").where("timestamp", ">=", two_weeks_ago).get()
+    #two_weeks_ago = current_time - timedelta(days=14)
+    #return database.collection("orders").where("timestamp", ">=", two_weeks_ago).get()
+    return database.collection("orders").get()
+
+
+@app.route('/data')
+def get_data():
+    userId = request.args.get('uid')
+    database = get_firestore_database()
+    user_doc = database.collection('users').document(userId).get()
+    restaurant_id = user_doc.get("restaurantId")
+    restaurant_doc = database.collection('restaurants').document(restaurant_id).get()
+
+    if not restaurant_doc.exists:
+        return jsonify({"error": "Restaurant not found"}), 404
+
+    # Filter orders by restaurant ID before passing to the function
+    orders = database.collection('orders').where("restaurantId", "==", restaurant_id).get()
+    restaurant_amounts = get_running_total_for_restaurant(orders, database, restaurant_id)
+
+    if restaurant_amounts is None:
+        return jsonify({"error": "Invalid restaurant ID"}), 404
+
+    restaurant_data = {
+        "name": restaurant_doc.get("name"),
+        "data": restaurant_amounts.get("data", []),
+        "totalAmount": restaurant_amounts.get("amount", 0)
+    }
+
+    return jsonify([restaurant_data])
+
+
+
+
+def get_running_total_for_restaurant(orders, database, restaurant_id):
+    restaurant_amounts = {}
+
+    restaurant = get_restaurant(database, restaurant_id)
+    if restaurant is None:
+        return None
+
+    restaurant_name = restaurant.get("name", None)
+
+    for order in orders:
+        order_dict = order.to_dict()
+        amount = order_dict["amount"]
+        timestamp = order_dict["timestamp"]
+        if restaurant_name:
+            if restaurant_name in restaurant_amounts:
+                restaurant_amounts[restaurant_name]["data"].append({"x": timestamp.timestamp(), "y": amount})
+                restaurant_amounts[restaurant_name]["amount"] += amount
+            else:
+                restaurant_amounts[restaurant_name] = {"data": [{"x": timestamp.timestamp(), "y": amount}], "amount": amount}
+
+    # Sort the data points by timestamp and compute the cumulative total
+    for restaurant_name, data in restaurant_amounts.items():
+        data["data"].sort(key=lambda point: point["x"])
+        cumulative_total = 0
+        for point in data["data"]:
+            cumulative_total += point["y"]
+            point["y"] = cumulative_total
+
+    return restaurant_amounts.get(restaurant_name, None)
+
 
 def get_restaurant(database, restaurant_id):
     restaurant_ref = database.collection("restaurants").document(restaurant_id)
     restaurant_doc = restaurant_ref.get()
-
     return restaurant_doc.to_dict() if restaurant_doc.exists else None
 
-def group_orders_by_restaurant(orders, database):
+
+
+
+@app.route('/abc')
+def render_login():
+    return render_template('login.html')
+
+
+@app.route('/')
+def login():
+    return render_template('signup.html')
+
+
+@app.route('/11')
+def render_signUp():
+    return render_template('index.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    # Get the form data from the request
+    email = request.form['email']
+    password = request.form['password']
+    restaurant_id = request.form['restaurantId']
+
+    # Create a Firestore client
+    db = firestore.client()
+
+    # Create a new user document in the "users" collection
+    user_data = {
+        'email': email,
+        'password': password,
+        'restaurantId': restaurant_id,
+        'createdAt': datetime.now()
+    }
+    # db.collection('users').add(user_data)
+    # Create a new user in Firebase Authentication
+    auth = firebase_admin.auth
+    user = auth.create_user(email=email, password=password)
+    db.collection('users').document(user.uid).set(user_data)
+    # Add custom claims to the user indicating their restaurant ID
+    # auth.set_custom_user_claims(user.uid, {'restaurant_id': restaurant_id})
+
+    # Redirect the user to a success page
+    return redirect(url_for('index'))
+
+def restaurant_to_dict(restaurant):
+    restaurant_dict = restaurant.to_dict()
+
+    if "location" in restaurant_dict:
+        restaurant_dict["location"] = {
+            "latitude": restaurant_dict["location"].latitude,
+            "longitude": restaurant_dict["location"].longitude
+        }
+
+    return restaurant_dict
+
+#Put request to update orders left
+# Function to update ordersLeft attribute of a restaurant
+@app.route("/restaurants/<restaurant_id>", methods=["PUT"])
+def update_orders_left(restaurant_id):
+    firestore_db = get_firestore_database()
+    restaurant_ref = firestore_db.collection("restaurants").document(str(restaurant_id))
+    restaurant = restaurant_ref.get()
+
+    if not restaurant.exists:
+        return jsonify({"error": "Restaurant not found"}), 404
+
+    try:
+        data = request.get_json()
+        orders_left = int(data["ordersLeft"])
+        if(orders_left < 0) or (orders_left > 20):
+       
+            return jsonify({"error": "Cannot process this many orders"}), 400
+ 
+    except (ValueError, KeyError, TypeError):
+        return jsonify({"error": "Invalid data provided"}), 400
+
+    restaurant_ref.update({"ordersLeft": orders_left})
+
+    updated_restaurant = restaurant_to_dict(restaurant_ref.get())
+    return jsonify({"success": True, "restaurant": updated_restaurant}), 200
+
+
+""" def group_orders_by_restaurant(orders, database):
     restaurant_amounts = {}
     for order in orders:
         order_dict = order.to_dict()
@@ -55,64 +203,15 @@ def group_orders_by_restaurant(orders, database):
             cumulative_total += point["y"]
             point["y"] = cumulative_total
 
-    return restaurant_amounts
+    return restaurant_amounts """
 
-@app.route('/data')
-def get_data():
-    database = get_firestore_database()
-    orders = get_orders(database)
-    restaurant_amounts = group_orders_by_restaurant(orders, database)
-    restaurant_data = []
-    for restaurant_name, data in restaurant_amounts.items():
-        restaurant_data.append({
-            "name": restaurant_name,
-            "data": data["data"],
-            "totalAmount": data["amount"]
-        })
-    response = Response(json.dumps(restaurant_data), content_type='application/json;charset=utf-8')
-    return response
+""" def get_restaurant(database, restaurant_id):
+    restaurant_ref = database.collection("restaurants").document(restaurant_id)
+    restaurant_doc = restaurant_ref.get()
 
-@app.route('/abc')
-def render_login():
-    return render_template('login.html')
-
-@app.route('/')
-def login():
-    return render_template('signup.html')
+    return restaurant_doc.to_dict() if restaurant_doc.exists else None """
 
 
-
-@app.route('/11')
-def render_signUp():
-    return render_template('index.html')
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    # Get the form data from the request
-    email = request.form['email']
-    password = request.form['password']
-    restaurant_id = request.form['restaurantId']
-    
-    # Create a Firestore client
-    db = firestore.client()
-    
-    # Create a new user document in the "users" collection
-    user_data = {
-        'email': email,
-        'password': password,
-        'restaurantId': restaurant_id,
-        'createdAt': datetime.now()
-    }
-    #db.collection('users').add(user_data)
-    #Create a new user in Firebase Authentication
-    auth = firebase_admin.auth
-    user = auth.create_user(email=email, password=password)
-    db.collection('users').document(user.uid).set(user_data)
-    # Add custom claims to the user indicating their restaurant ID
-    #auth.set_custom_user_claims(user.uid, {'restaurant_id': restaurant_id})
-    
-    # Redirect the user to a success page
-    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- restaurantportal/static/main.js
+++ restaurantportal/static/main.js
@@ -1,28 +1,45 @@
-async function fetchData() {
-  const response = await fetch('/data');
+
+let uid = null;
+let value = 0;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzrBi9zuKrJDnZowoR9s9jJPqZX2egXUA",
+  authDomain: "lastbite-907b1.firebaseapp.com",
+  projectId: "lastbite-907b1",
+  storageBucket: "lastbite-907b1.appspot.com",
+  messagingSenderId: "342058562429",
+  appId: "1:342058562429:web:f2fef939342130176f1214",
+  measurementId: "G-9Z27YN7XJX"
+};
+firebase.initializeApp(firebaseConfig);
+
+async function fetchRestaurantOrders(uid) {
+  const response = await fetch(`/data?uid=${uid}`);
   const data = await response.json();
   return data;
 }
 
 function createChart(data) {
   const ctx = document.getElementById('chart').getContext('2d');
+  const datasets = [];
+  for (const restaurant of data) {
+    datasets.push({
+      label: restaurant.name,
+      data: restaurant.data ? restaurant.data.map((point) => ({
+        x: new Date(point.x * 1000),
+        y: point.y,
+      })) : [],
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1,
+      pointBackgroundColor: 'black',
+      fill: false,
+    });
+  }
   new Chart(ctx, {
-    type: 'line', // Change the chart type to 'line'
+    type: 'line',
     data: {
-      datasets: data.map(restaurant => {
-        return {
-          label: restaurant.name,
-          data: restaurant.data.map(point => ({
-            x: new Date(point.x * 1000), // Convert timestamp to Date object
-            y: point.y,
-          })),
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
-          pointBackgroundColor: 'black', // Set the color of the points
-          fill: false, // Set fill to false for line chart
-        };
-      }),
+      datasets,
     },
     options: {
       scales: {
@@ -50,12 +67,130 @@ function createChart(data) {
     },
   });
 }
-async function main() {
-  const data = await fetchData();
-  const canvas = document.createElement('canvas');
-  canvas.id = 'chart';
-  document.getElementById('canvas-container').appendChild(canvas);
-  createChart(data);
+
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    console.log("Current user:", user.email);
+    uid = user.uid;
+  } else {
+    console.log("No user logged in");
+  }
+});
+
+
+function submitValue() {
+  const firestore = firebase.firestore();
+  const ordersLeft = ordersLeftValue.innerHTML;
+
+  const userId = firebase.auth().currentUser.uid;
+
+  firestore.collection("users").doc(userId).get().then((doc) => {
+    if (doc.exists) {
+      const restaurantId = doc.data().restaurantId;
+      console.log(restaurantId);
+      // Submit the value here
+      fetch(`/restaurants/${restaurantId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          ordersLeft: ordersLeft
+        })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          alert("Value submitted: " + ordersLeft);
+        });
+
+    } else {
+      console.log("No such document!");
+    }
+  }).catch((error) => {
+    console.log("Error getting document:", error);
+  });
 }
 
-main();
+
+let ordersLeftValue;
+
+document.addEventListener('DOMContentLoaded', () => {
+  ordersLeftValue = document.getElementById('orders_left_value');
+});
+
+function decrease() {
+  value--;
+  console.log(value);
+  if (ordersLeftValue) {
+    updateValue();
+  }
+}
+
+function increase() {
+  value++;
+  console.log(value);
+  if (ordersLeftValue) {
+    updateValue();
+  }
+}
+
+function updateValue() {
+  ordersLeftValue.innerHTML = value;
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    console.log("Current user:", user.email);
+    const firestore = firebase.firestore();
+    const userId = user.uid;
+    firestore.collection("users").doc(userId).get().then((doc) => {
+      if (doc.exists) {
+        const restaurantId = doc.data().restaurantId;
+        firestore.collection("restaurants").doc(restaurantId).get().then((doc) => {
+          if (doc.exists) {
+            const ordersLeft = doc.data().ordersLeft;
+            let value = ordersLeft;
+            document.getElementById("orders_left_value").innerText = ordersLeft;
+          } else {
+            console.log("No such restaurant document!");
+          }
+        }).catch((error) => {
+          console.log("Error getting restaurant document:", error);
+        });
+      } else {
+        console.log("No such user document!");
+      }
+    }).catch((error) => {
+      console.log("Error getting user document:", error);
+    });
+  } else {
+    console.log("No user logged in");
+  }
+});
+
+
+async function main() {
+  setTimeout(1000);
+  if (uid) {
+    console.log(uid);
+    const data = await fetchRestaurantOrders(uid);
+    const canvas = document.createElement('canvas');
+    if (data.length > 0) {
+      // Set the total amount text
+      document.getElementById("total-amount").innerText = `Total Amount: $${data[0].totalAmount.toFixed(2)}`;
+    }
+
+    canvas.id = 'chart';
+    document.getElementById('canvas-container').appendChild(canvas);
+    
+    createChart(data);
+  } else {
+    // No user is signed in.
+    // Redirect to login page or show a message
+  }
+}
+
+setTimeout(main, 1000);
